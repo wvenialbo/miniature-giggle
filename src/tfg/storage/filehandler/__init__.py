d8756717b47707378765265e18ca08b5f8d64e62
wvenialbo <wvenialbo@gmail.com>
2026-01-11 11:53:34 -0300

    remove old file handlers

--- src/tfg/storage/filehandler/__init__.py
+++
@@ -1,5 +0,0 @@
-from .file_handler import FileHandler
-from .numpy_file_handler import NumpyFileHandler
-from .pickle_file_handler import PickleFileHandler  # nosec
-
-__all__ = ["FileHandler", "NumpyFileHandler", "PickleFileHandler"]
