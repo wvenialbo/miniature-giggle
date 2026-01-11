ac9fb6d8e5776e200f7977f0646cd4cba5bf722e
wvenialbo <wvenialbo@gmail.com>
2026-01-11 03:15:07 -0300

    Add # nosec marker to pickle file handler import in __init__.py.

--- src/tfg/storage/filehandler/__init__.py
+++ src/tfg/storage/filehandler/__init__.py
@@ -1,5 +1,5 @@
 from .file_handler_interface import FileHandlerInterface
 from .numpy_file_handler import NumpyFileHandler
-from .pickle_file_handler import PickleFileHandler
+from .pickle_file_handler import PickleFileHandler  # nosec
 
 __all__ = ["FileHandlerInterface", "NumpyFileHandler", "PickleFileHandler"]
