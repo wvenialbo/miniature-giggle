5c95082269a9b3ae5ece5360a4e26f41c18cb3a9
wvenialbo <wvenialbo@gmail.com>
2026-01-11 03:13:08 -0300

    Add file handler for numpy, pickle files and update __init__.py to include it.

--- src/tfg/storage/filehandler/__init__.py
+++ src/tfg/storage/filehandler/__init__.py
@@ -1,6 +1,5 @@
-from .file_handler_interface import (
-    FileHandlerInterface as FileHandlerInterface,
-)
+from .file_handler_interface import FileHandlerInterface
+from .numpy import NumpyFileHandler
+from .pickle_file_handler import PickleFileHandler
 
-
-__all__ 
+__all__ = ["FileHandlerInterface", "NumpyFileHandler", "PickleFileHandler"]
---
+++ src/tfg/storage/filehandler/pickle_file_handler.py
@@ -0,0 +1,62 @@
+import pickle  # nosec
+import typing as tp
+import warnings
+
+
+class PickleFileHandler:
+    """
+    Clase para manejar la carga y guardado de archivos pickle.
+
+    Methods
+    -------
+    load(filename: str) -> Any
+        Carga datos desde un archivo pickle.
+    save(data: Any, filename: str) -> None
+        Guarda datos en un archivo pickle.
+    """
+
+    @staticmethod
+    def _issue_warning() -> None:
+        warnings.warn(
+            "El uso de archivos pickle puede ser inseguro. "
+            "Asegúrese de que el archivo provenga de una "
+            "fuente confiable",
+            UserWarning,
+        )
+
+    def load(self, *, filename: str) -> tp.Any:
+        """
+        Carga datos desde un archivo pickle.
+
+        Parameters
+        ----------
+        filename : str
+            Nombre del archivo.
+
+        Returns
+        -------
+        Any
+            Datos cargados desde el archivo.
+        """
+        self._issue_warning()
+        with open(filename, "rb") as file:
+            return pickle.load(file)  # nosec
+
+    def save(self, *, data: tp.Any, filename: str) -> None:
+        """
+        Guarda datos en un archivo pickle.
+
+        Parameters
+        ----------
+        data : Any
+            Datos a guardar.
+        filename : str
+            Nombre del archivo.
+
+        Returns
+        -------
+        None
+        """
+        self._issue_warning()
+        with open(filename, "wb") as file:
+            pickle.dump(data, file)  # nosec
