666c9253fc35e8e229dd1c0db71f2859dab74efc
wvenialbo <wvenialbo@gmail.com>
2026-01-11 03:14:07 -0300

    Rename numpy file handler module and update __init__.py accordingly.

--- src/tfg/storage/filehandler/__init__.py
+++ src/tfg/storage/filehandler/__init__.py
@@ -1,5 +1,5 @@
 from .file_handler_interface import FileHandlerInterface
-from .numpy import NumpyFileHandler
+from .numpy_file_handler import NumpyFileHandler
 from .pickle_file_handler import PickleFileHandler
 
 __all__ = ["FileHandlerInterface", "NumpyFileHandler", "PickleFileHandler"]
