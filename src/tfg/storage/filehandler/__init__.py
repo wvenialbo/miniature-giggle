ed0bb1eac5095e52829d594035e193d9c036d9b5
wvenialbo <wvenialbo@gmail.com>
2026-01-11 04:18:32 -0300

    rename file_handler_interface.py to file_handler.py and update __init__.py imports accordingly.

--- src/tfg/storage/filehandler/__init__.py
+++ src/tfg/storage/filehandler/__init__.py
@@ -1,4 +1,4 @@
-from .file_handler_interface import FileHandlerInterface
+from .file_handler import FileHandlerInterface
 from .numpy_file_handler import NumpyFileHandler
 from .pickle_file_handler import PickleFileHandler  # nosec
 
