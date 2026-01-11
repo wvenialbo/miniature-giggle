77e0c89efe7469f93f7122ca3d1e8b1cfbc5e302
wvenialbo <wvenialbo@gmail.com>
2026-01-11 03:18:09 -0300

    Update draft code based on recent edits.

--- src/tfg/datafiles.py
+++ src/tfg/datafiles.py
@@ -1,7 +1,6 @@
 import dataclasses as dc
 import json
 import os
-import pickle  # nosec
 import typing as tp
 import warnings
 
@@ -9,118 +8,8 @@ import numpy as np
 import numpy.typing as npt
 from google import colab
 
+from .storage.filehandler import FileHandlerInterface
 from .utils import format_report, format_table, get_columns_size, to_indices
-
-
-class FileHandlerInterface(tp.Protocol):
-    def load(self, *, filename: str) -> tp.Any: ...
-
-    def save(self, *, data: tp.Any, filename: str) -> None: ...
-
-
-class NumpyFileHandler:
-    """
-    Clase para manejar la carga y guardado de archivos npz.
-
-    Methods
-    -------
-    load(filename: str) -> Any
-        Carga datos desde un archivo npz.
-    save(data: Any, filename: str) -> None
-        Guarda datos en un archivo npz.
-    """
-
-    def load(self, *, filename: str) -> tp.Any:
-        """
-        Carga datos desde un archivo npz.
-
-        Parameters
-        ----------
-        filename : str
-            Nombre del archivo.
-
-        Returns
-        -------
-        Any
-            Datos cargados desde el archivo.
-        """
-        return np.load(filename, allow_pickle=False)
-
-    def save(self, *, data: tp.Any, filename: str) -> None:
-        """
-        Guarda datos en un archivo npz.
-
-        Parameters
-        ----------
-        data : Any
-            Datos a guardar.
-        filename : str
-            Nombre del archivo.
-
-        Returns
-        -------
-        None
-        """
-        np.savez_compressed(filename, **data)
-
-
-class PickleFileHandler:
-    """
-    Clase para manejar la carga y guardado de archivos pickle.
-
-    Methods
-    -------
-    load(filename: str) -> Any
-        Carga datos desde un archivo pickle.
-    save(data: Any, filename: str) -> None
-        Guarda datos en un archivo pickle.
-    """
-
-    @staticmethod
-    def _issue_warning() -> None:
-        warnings.warn(
-            "El uso de archivos pickle puede ser inseguro. "
-            "Asegúrese de que el archivo provenga de una "
-            "fuente confiable",
-            UserWarning,
-        )
-
-    def load(self, *, filename: str) -> tp.Any:
-        """
-        Carga datos desde un archivo pickle.
-
-        Parameters
-        ----------
-        filename : str
-            Nombre del archivo.
-
-        Returns
-        -------
-        Any
-            Datos cargados desde el archivo.
-        """
-        self._issue_warning()
-        with open(filename, "rb") as file:
-            return pickle.load(file)  # nosec
-
-    def save(self, *, data: tp.Any, filename: str) -> None:
-        """
-        Guarda datos en un archivo pickle.
-
-        Parameters
-        ----------
-        data : Any
-            Datos a guardar.
-        filename : str
-            Nombre del archivo.
-
-        Returns
-        -------
-        None
-        """
-        self._issue_warning()
-        with open(filename, "wb") as file:
-            pickle.dump(data, file)  # nosec
 
 
 class GoogleDriver:
