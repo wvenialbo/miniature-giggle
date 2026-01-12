574df0451df22bf0127e94e028caf22c54a75c6e
wvenialbo <wvenialbo@gmail.com>
2026-01-12 02:58:46 -0300

    remove filesystem subpackages

--- src/tfg/storage/filesystem/__init__.py
+++
@@ -1,9 +0,0 @@
-from .colab_filesystem_mounter import ColabFileSystemMounter
-from .filesystem_mounter import FileSystemMounter
-from .local_filesystem_mounter import LocalFileSystemMounter
-
-__all__ = [
-    "ColabFileSystemMounter",
-    "FileSystemMounter",
-    "LocalFileSystemMounter",
-]
