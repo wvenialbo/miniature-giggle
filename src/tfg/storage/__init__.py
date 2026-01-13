9265251b08287a5a7a60102b84788a354e80715a
wvenialbo <wvenialbo@gmail.com>
2026-01-13 06:55:31 -0300

    update handler imports

--- src/tfg/storage/__init__.py
+++ src/tfg/storage/__init__.py
@@ -1,3 +1,13 @@
-from . import backend, connector, core, handler, uri
+from . import backend, connector, core, datasource, handler, uri
+from .core.factory import get, register_factory
 
-__all__ = ["backend", "connector", "core", "handler", "uri"]
+__all__ = [
+    "backend",
+    "connector",
+    "core",
+    "datasource",
+    "get",
+    "handler",
+    "register_factory",
+    "uri",
+]
--- src/tfg/storage/handler/__init__.py
+++ src/tfg/storage/handler/__init__.py
@@ -1,5 +1,17 @@
 from .base import DataHandler
+from .csv import CSVHandler
+from .json import JSONHandler
+from .njson import NJSONHandler
 from .numpy import NumpyHandler
 from .pickle import PickleHandler  # nosec
+from .yaml import YAMLHandler
 
-__all__ = ["DataHandler", "NumpyHandler", "PickleHandler"]
+__all__ = [
+    "CSVHandler",
+    "DataHandler",
+    "JSONHandler",
+    "NJSONHandler",
+    "NumpyHandler",
+    "PickleHandler",
+    "YAMLHandler",
+]
