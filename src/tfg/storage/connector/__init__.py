bf3ba341c3423e484180f91563313f016a899ac1
wvenialbo <wvenialbo@gmail.com>
2026-01-13 11:26:04 -0300

    remove obsolete connector classes

--- src/tfg/storage/connector/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import ConnectionManager
-from .colab import ColabDriveConnectionManager
-from .local import LocalConnectionManager
-
-__all__ = [
-    "ConnectionManager",
-    "ColabDriveConnectionManager",
-    "LocalConnectionManager",
-]
--- src/tfg/storage/core/__init__.py
+++ src/tfg/storage/core/__init__.py
@@ -1,9 +1,5 @@
-from .factory import (
-    reset_default_handlers,
-    set_default_handlers,
-    use_drive_for_colab,
-    use_local_drive,
-)
+from .factory import use_drive_for_colab, use_local_drive
+from .handlers import reset_default_handlers, set_default_handlers
 
 __all__ = [
     "reset_default_handlers",
