b0fba2759dc3c852c8e9fb1976eb72c90f3e81fd
wvenialbo <wvenialbo@gmail.com>
2026-01-13 11:40:49 -0300

    remap imports

--- src/tfg/storage/__init__.py
+++ src/tfg/storage/__init__.py
@@ -1,9 +1,10 @@
 from . import backend, connector, core, datasource, handler, uri
-from .core.factory import (
-    get,
-    register_factory,
+from .core import (
+    close_use_drive_for_colab,
     reset_default_handlers,
     set_default_handlers,
+    use_drive_for_colab,
+    use_local_drive,
 )
 
 __all__ = [
@@ -11,10 +12,11 @@ __all__ = [
     "connector",
     "core",
     "datasource",
-    "get",
     "handler",
-    "register_factory",
     "reset_default_handlers",
     "set_default_handlers",
     "uri",
+    "use_drive_for_colab",
+    "close_use_drive_for_colab",
+    "use_local_drive",
 ]
--- src/tfg/storage/core/__init__.py
+++ src/tfg/storage/core/__init__.py
@@ -1,7 +1,9 @@
-from .factory import use_drive_for_colab, use_local_drive
+from .colab import close_use_drive_for_colab, use_drive_for_colab
 from .handlers import reset_default_handlers, set_default_handlers
+from .local import use_local_drive
 
 __all__ = [
+    "close_use_drive_for_colab",
     "reset_default_handlers",
     "set_default_handlers",
     "use_drive_for_colab",
