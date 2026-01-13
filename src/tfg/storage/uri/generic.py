e06114298dc5ef08cd45468c895764cb4c9db7fc
wvenialbo <wvenialbo@gmail.com>
2026-01-13 02:01:07 -0300

    update GenericURIMapper

--- src/tfg/storage/uri/generic.py
+++ src/tfg/storage/uri/generic.py
@@ -5,30 +5,52 @@ from .base import URIMapper
 
 class GenericURIMapper(URIMapper):
     """
-    Transforma URIs genéricas respecto a una ruta base.
+    Transforma URI genéricas respecto a una ruta base.
+
+    Convierte entre URI lógicas y respecto a una ruta base especificada
+    como raíz.  La finalidad es proporcionar una abstracción simple para
+    manejar rutas relativas dentro de un espacio de nombres definido.
+    Este mapeador no realiza conversiones complejas, sino que
+    simplemente ajusta las rutas relativas a una base fija.  Es usado en
+    componentes de alto nivel que requieren una referencia común para
+    las rutas, como, por ejemplo, en manejadores de repositorios.
+
+    Los mapeadores de URI permiten que los backends almacenen datos en
+    ubicaciones nativas específicas del backend, mientras exponen rutas
+    genéricas logicas para el usuario.  Esto es útil para backends que
+    requieren estructuras de URI específicas o prefijos.
+
+    Se adopta el formato POSIX/Unix para las URI lógicas, utilizando '/'
+    como separador de componentes de rutas.
 
     Parameters
     ----------
-    path : str
-        Ruta base para las URIs lógicas.
+    base_path : str
+        Ruta base para las URI genéricas.  Debe ser una ruta genérica
+        válida en el backend. Puede ser relativa o absoluta y es
+        resuelta a una ruta absoluta durante la inicialización.
+
+    Attributes
+    ----------
+    base_path : pathlib.Path
+        La ruta base absoluta utilizada para las conversiones de URI.
+        Es una ruta en formato POSIX.
 
     Methods
     -------
+    to_generic(uri: str) -> str
+        Convierte una URI nativa a una URI genérica.
     to_native(uri: str) -> str
-        Convierte una URI lógica a una URI nativa.
-    to_logical(uri: str) -> str
-        Convierte una URI nativa del backend a una lógica.
+        Convierte una URI genérica a una URI nativa.
     """
 
-    def __init__(self, path: str) -> None:
-        base_path = pl.PosixPath(f"/{path.lstrip('/')}")
-        self.base_path = base_path.resolve(strict=False)
+    def __init__(self, base_path: str) -> None:
+        cwd = pl.Path().resolve(strict=False)
+        crd = cwd / base_path
+        self.base_path = f"/{crd.relative_to(crd.anchor).as_posix()}"
 
     def __repr__(self) -> str:
-        return f"GenericURIMapper(path='{self.base_path}')"
-
-    def __str__(self) -> str:
-        return f"{self.base_path}"
+        return f"GenericURIMapper(base_path='{self.base_path}')"
 
     def to_logical(self, uri: str) -> str:
         """
@@ -44,9 +66,9 @@ class GenericURIMapper(URIMapper):
         str
             La URI lógica transformada para el usuario.
         """
-        path = pl.PosixPath(uri).resolve(strict=False)
-        relative_path = path.relative_to(self.base_path)
-        return str(relative_path)
+        native_root = pl.PurePosixPath(self.base_path)
+        generic_path = pl.PurePosixPath(uri).relative_to(native_root)
+        return str(generic_path)
 
     def to_native(self, uri: str) -> str:
         """
@@ -62,7 +84,6 @@ class GenericURIMapper(URIMapper):
         str
             La URI nativa transformada para el backend.
         """
-        stem = pl.PosixPath(f"/{uri.lstrip('/')}")
-        path = str(stem.resolve(strict=False))
-        target = self.base_path / path.lstrip("/")
-        return str(target.resolve(strict=False))
+        generic_root = pl.PurePosixPath(self.base_path)
+        native_path = generic_root / uri.lstrip("/")
+        return str(native_path)
