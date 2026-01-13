a9047aca9ee08c637a729daef167d22223e24409
wvenialbo <wvenialbo@gmail.com>
2026-01-13 00:17:36 -0300

    update protocol documentation

--- src/tfg/storage/uri/base.py
+++ src/tfg/storage/uri/base.py
@@ -3,24 +3,27 @@ import typing as tp
 
 class URIMapper(tp.Protocol):
     """
-    Transforma entre URIs lógicas (del usuario) y nativas (del backend).
+    Transforma entre URIs genéricas y nativas del backend.
 
     Los mapeadores de URI permiten que los backends almacenen datos en
-    ubicaciones específicas del backend, mientras exponen URIs lógicas
-    consistentes al usuario.  Esto es útil para backends que requieren
-    estructuras de URI específicas o prefijos.
+    ubicaciones nativas específicas del backend, mientras exponen rutas
+    genéricas logicas para el usuario.  Esto es útil para backends que
+    requieren estructuras de URI específicas o prefijos.
+
+    Se adopta el formato POSIX/Unix para las URI lógicas, utilizando '/'
+    como separador de componentes de rutas.
 
     Methods
     -------
+    to_generic(uri: str) -> str
+        Convierte una URI nativa a una URI genérica.
     to_native(uri: str) -> str
-        Convierte una URI lógica a una URI nativa.
-    to_logical(uri: str) -> str
-        Convierte una URI nativa del backend a una lógica.
+        Convierte una URI genérica a una URI nativa.
     """
 
-    def to_logical(self, *, uri: str) -> str:
+    def to_generic(self, uri: str) -> str:
         """
-        Convierte una URI nativa a una lógica.
+        Convierte una URI genérica a una URI nativa.
 
         Parameters
         ----------
@@ -34,9 +37,9 @@ class URIMapper(tp.Protocol):
         """
         ...
 
-    def to_native(self, *, uri: str) -> str:
+    def to_native(self, uri: str) -> str:
         """
-        Convierte una URI lógica a una URI nativa.
+        Convierte una URI genérica a una URI nativa.
 
         Parameters
         ----------
