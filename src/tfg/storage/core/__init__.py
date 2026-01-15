1c9cc394aaf7f121a2b6c68489181dc3147c35b3
wvenialbo <wvenialbo@gmail.com>
2026-01-15 11:22:13 -0300

    update imports in core module

--- src/tfg/storage/core/__init__.py
+++ src/tfg/storage/core/__init__.py
@@ -1,4 +1,6 @@
+from .aws import use_aws
 from .colab import close_colab_drive, use_colab_drive
+from .google import use_google_drive
 from .handlers import reset_default_handlers, set_default_handlers
 from .local import use_local_drive
 
@@ -6,6 +8,8 @@ __all__ = [
     "close_colab_drive",
     "reset_default_handlers",
     "set_default_handlers",
+    "use_aws",
     "use_colab_drive",
+    "use_google_drive",
     "use_local_drive",
 ]
