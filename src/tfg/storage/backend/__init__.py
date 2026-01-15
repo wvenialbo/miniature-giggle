758685284cd18712ea948d71e6e65d1ad7da03ec
wvenialbo <wvenialbo@gmail.com>
2026-01-15 04:49:32 -0300

    fix GoogleDriveBackend import at module level

--- src/tfg/storage/backend/__init__.py
+++ src/tfg/storage/backend/__init__.py
@@ -1,5 +1,5 @@
 from .base import StorageBackend
 from .filesystem import FilesystemBackend
-from .google import GoogleDriveAPIBackend
+from .google import GoogleDriveBackend
 
-__all__ = ["StorageBackend", "FilesystemBackend", "GoogleDriveAPIBackend"]
+__all__ = ["StorageBackend", "FilesystemBackend", "GoogleDriveBackend"]
