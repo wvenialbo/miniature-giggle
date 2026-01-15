0df34575e57e45e93a6bc1e29ac18853a5d969aa
wvenialbo <wvenialbo@gmail.com>
2026-01-15 11:56:33 -0300

    update AWSURIMapper to not use base_prefix anymore

--- src/tfg/storage/mapper/aws.py
+++ src/tfg/storage/mapper/aws.py
@@ -13,10 +13,8 @@ class AWSURIMapper(URIMapper):
     s3://bucket/prefix/path.
     """
 
-    def __init__(self, bucket: str, base_prefix: str = "") -> None:
+    def __init__(self, bucket: str) -> None:
         self.bucket = bucket
-        # Normalizamos el prefijo para que no termine en / innecesariamente
-        self.base_prefix = base_prefix.strip(POSIX_SEPARATOR)
 
     def to_generic(self, uri: str) -> str:
         # uri: s3://my-bucket/base/data/file.csv -> /data/file.csv
@@ -27,13 +25,10 @@ class AWSURIMapper(URIMapper):
             )
 
         path = uri[len(prefix) :]
-        if self.base_prefix and path.startswith(self.base_prefix):
-            path = path[len(self.base_prefix) :]
 
         return f"{POSIX_SEPARATOR}{path.lstrip(POSIX_SEPARATOR)}"
 
     def to_native(self, uri: str) -> str:
         # uri: /data/file.csv -> s3://my-bucket/base/data/file.csv
         clean_path = uri.lstrip(POSIX_SEPARATOR)
-        full_key = f"{self.base_prefix}{S3_SEPARATOR}{clean_path}"
-        return f"{S3_PREFIX}{self.bucket}{S3_SEPARATOR}{full_key}"
+        return f"{S3_PREFIX}{self.bucket}{S3_SEPARATOR}{clean_path}"
