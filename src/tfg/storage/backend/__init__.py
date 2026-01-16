11176bb67ee8e5b34ad81cb1d21fddbf6857c0cc
wvenialbo <wvenialbo@gmail.com>
2026-01-16 06:47:49 -0300

    update imports to include GCS modules (GCSBackend and GCSURIMapper)

--- src/tfg/storage/backend/__init__.py
+++ src/tfg/storage/backend/__init__.py
@@ -1,11 +1,13 @@
 from .aws import AWSBackend
 from .base import StorageBackend
 from .filesystem import FilesystemBackend
+from .gcs import GCSBackend
 from .google import GoogleDriveBackend
 
 __all__ = [
     "AWSBackend",
     "FilesystemBackend",
+    "GCSBackend",
     "GoogleDriveBackend",
     "StorageBackend",
 ]
--- src/tfg/storage/mapper/__init__.py
+++ src/tfg/storage/mapper/__init__.py
@@ -1,11 +1,13 @@
 from .aws import AWSURIMapper
 from .base import URIMapper
+from .gcs import GCSURIMapper
 from .generic import GenericURIMapper
 from .google import GoogleDriveURIMapper
 from .path import PathURIMapper
 
 __all__ = [
     "AWSURIMapper",
+    "GCSURIMapper",
     "GenericURIMapper",
     "GoogleDriveURIMapper",
     "PathURIMapper",
