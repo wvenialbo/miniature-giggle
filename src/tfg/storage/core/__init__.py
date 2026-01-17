624272b088ebde9981f40b24fff4cb84c9708c90
wvenialbo <wvenialbo@gmail.com>
2026-01-17 12:34:54 -0300

    add use_kaggle_dataset factory

--- src/tfg/storage/core/__init__.py
+++ src/tfg/storage/core/__init__.py
@@ -2,6 +2,7 @@ from .aws import use_aws_cloud
 from .colab import release_colab_drive, use_colab_drive
 from .gcs import use_gcs_cloud
 from .gdrive import use_google_drive
+from .kaggle import use_kaggle_dataset
 from .local import use_local_drive
 from .ncei import use_ncei_archive
 
@@ -11,6 +12,7 @@ __all__ = [
     "use_colab_drive",
     "use_gcs_cloud",
     "use_google_drive",
+    "use_kaggle_dataset",
     "use_local_drive",
     "use_ncei_archive",
 ]
