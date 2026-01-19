4fe7efec8b150e8813c08590c946519728835ca0
wvenialbo <wvenialbo@gmail.com>
2026-01-18 21:52:14 -0300

    update version handling to use importlib.metadata (v0.1.5-dev0)

--- src/tfg/info.py
+++ src/tfg/info.py
@@ -1,6 +1,15 @@
+import importlib.metadata
+
+try:
+    # "tfg-wvv" debe ser igual al 'name' en tu pyproject.toml
+    __version__ = importlib.metadata.version("tfg-wvv")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "v0.1.5-dev0"
+
 __package_id__ = "TFG-WVV"
+
 __package_name__ = (
     f"{__package_id__} — Código del proyecto de Trabajo de Fin de Grado"
 )
+
 __package_root__ = "tfg"
-__version__ = "v0.1-rc1"
