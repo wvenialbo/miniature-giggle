7cc96b90ada60995e38049d58a277437e4c68cc8
wvenialbo <wvenialbo@gmail.com>
2026-01-21 00:17:53 -0300

    refactor dataset loader and construction functions

--- src/tfg/dataset.py
+++ src/tfg/dataset.py
@@ -61,114 +61,107 @@ class Dataset:
         return to_indices(self.valid_mask)
 
 
-class DatasetLoader:
+def load_dataset(
+    *, file: StrOrBytesPath, dtype: npt.DTypeLike | None = None
+) -> Dataset:
+    data = np.load(file=file, allow_pickle=False)
 
-    def __repr__(self) -> str:
-        return "DatasetRepository()"
+    metainfo = _extract_info(data)
 
-    def load(
-        self,
-        *,
-        file: StrOrBytesPath,
-        dtype: npt.DTypeLike | None = None,
-    ) -> Dataset:
-        data = np.load(file=file, allow_pickle=False)
+    return _construct_dataset(metainfo, dtype)
 
-        metainfo = self._extract_info(data)
 
-        return self._construct_dataset(metainfo, dtype)
+def _construct_dataset(
+    metainfo: DatasetInfo, dtype: npt.DTypeLike | None
+) -> Dataset:
+    valid_indices = to_indices(metainfo.valid_mask)
 
-    @classmethod
-    def _construct_dataset(
-        cls, metainfo: DatasetInfo, dtype: npt.DTypeLike | None
-    ) -> Dataset:
-        metadata: dict[str, tp.Any] = dc.asdict(metainfo)
+    metadata = _construct_metadata(metainfo, valid_indices)
 
-        n_samples, n_series = metainfo.timeseries_shape
+    timeseries = _construct_timeseries(
+        metainfo.profiles, valid_indices, metainfo.timeseries_shape, dtype
+    )
 
-        valid_indices = to_indices(metainfo.valid_mask)
+    dataset: dict[str, tp.Any] = {
+        "timeseries": timeseries,
+        "valid_mask": metainfo.valid_mask,
+        "metadata": Metadata(**metadata),
+    }
 
-        filenames = cls._construct_list(
-            metainfo.filenames, valid_indices, n_samples
-        )
-        iso_dates = cls._construct_list(
-            metainfo.dates, valid_indices, n_samples
-        )
+    return Dataset(**dataset)
 
-        metadata["n_samples"] = n_samples
-        metadata["n_series"] = n_series
-        metadata["filenames"] = filenames
-        metadata["iso_dates"] = iso_dates
 
-        del metadata["dates"]
-        del metadata["profiles"]
-        del metadata["valid_mask"]
-        del metadata["timeseries_shape"]
+def _construct_list(
+    rawdata: list[str], indices: npt.NDArray[np.intp], length: int
+) -> list[str]:
+    dataset = [""] * length
+    for i, index in enumerate(indices):
+        dataset[index] = rawdata[i]
 
-        timeseries = cls._construct_timeseries(
-            metainfo.profiles, valid_indices, metainfo.timeseries_shape, dtype
-        )
+    return dataset
 
-        dataset: dict[str, tp.Any] = {
-            "timeseries": timeseries,
-            "valid_mask": metainfo.valid_mask,
-            "metadata": Metadata(**metadata),
-        }
 
-        return Dataset(**dataset)
+def _construct_metadata(
+    metainfo: DatasetInfo, valid_indices: npt.NDArray[np.intp]
+) -> dict[str, tp.Any]:
+    metadata: dict[str, tp.Any] = dc.asdict(metainfo)
 
-    @staticmethod
-    def _construct_list(
-        rawdata: list[str], indices: npt.NDArray[np.intp], length: int
-    ) -> list[str]:
-        dataset = [""] * length
-        for i, index in enumerate(indices):
-            dataset[index] = rawdata[i]
+    n_samples, n_series = metainfo.timeseries_shape
 
-        return dataset
+    filenames = _construct_list(metainfo.filenames, valid_indices, n_samples)
+    iso_dates = _construct_list(metainfo.dates, valid_indices, n_samples)
 
-    @staticmethod
-    def _construct_timeseries(
-        profiles: npt.NDArray[np.floating],
-        indices: npt.NDArray[np.intp],
-        shape: tuple[int, int],
-        dtype: npt.DTypeLike | None,
-    ) -> npt.NDArray[np.floating]:
-        # Reconstruye las series temporales en una grilla temporal uniforme.
+    metadata["n_samples"] = n_samples
+    metadata["n_series"] = n_series
+    metadata["filenames"] = filenames
+    metadata["iso_dates"] = iso_dates
 
-        # Parameters
-        # ----------
-        # profiles : array float (n, m)
-        #     Array con n perfiles radiales de promedio acimutal, correspondientes a
-        #     n instantes de tiempo (no uniformemente distribuidos), y m mediciones
-        #     radiales, correspondientes a m series temporales.
-        # indices : array int (p,)
-        #     Array con los índices dónde colocar los perfiles en una grilla
-        #     temporal uniformemente distribuida (p >= n).
-        # shape : tuple[int, int]
-        #     Tupla con la forma de la serie temporal original.
+    del metadata["dates"]
+    del metadata["profiles"]
+    del metadata["valid_mask"]
+    del metadata["timeseries_shape"]
+    return metadata
 
-        # Return
-        # ------
-        # timeseries : array (m, p)
-        #     Array con m series temporales reconstruidas en una grilla temporal
-        #     uniforme, con NaN en los instantes donde no hay datos. Las filas
-        #     son series temporales, y las columnas son perfiles radiales.
-        ts_dtype = profiles.dtype if dtype is None else dtype
-        timeseries = np.full(shape, np.nan, dtype=ts_dtype)
-        timeseries[indices, :] = profiles.astype(ts_dtype)
 
-        return timeseries.T
+def _construct_timeseries(
+    profiles: npt.NDArray[np.floating],
+    indices: npt.NDArray[np.intp],
+    shape: tuple[int, int],
+    dtype: npt.DTypeLike | None,
+) -> npt.NDArray[np.floating]:
+    # Reconstruye las series temporales en una grilla temporal uniforme.
 
-    @staticmethod
-    def _extract_info(data: tp.Any) -> DatasetInfo:
-        metainfo: dict[str, tp.Any] = json.loads(str(data["metadata"][0]))
+    # Parameters
+    # ----------
+    # profiles : array float (n, m)
+    #     Array con n perfiles radiales de promedio acimutal, correspondientes a
+    #     n instantes de tiempo (no uniformemente distribuidos), y m mediciones
+    #     radiales, correspondientes a m series temporales.
+    # indices : array int (p,)
+    #     Array con los índices dónde colocar los perfiles en una grilla
+    #     temporal uniformemente distribuida (p >= n).
+    # shape : tuple[int, int]
+    #     Tupla con la forma de la serie temporal original.
 
-        metainfo["radii"] = np.asarray(metainfo["radii"], dtype=np.int32)
-        metainfo["profiles"] = np.asarray(data["profiles"], dtype=np.float32)
-        metainfo["valid_mask"] = np.asarray(
-            metainfo["valid_mask"], dtype=np.bool_
-        )
-        metainfo["timeseries_shape"] = tuple(metainfo["timeseries_shape"])
+    # Return
+    # ------
+    # timeseries : array (m, p)
+    #     Array con m series temporales reconstruidas en una grilla temporal
+    #     uniforme, con NaN en los instantes donde no hay datos. Las filas
+    #     son series temporales, y las columnas son perfiles radiales.
+    ts_dtype = profiles.dtype if dtype is None else dtype
+    timeseries = np.full(shape, np.nan, dtype=ts_dtype)
+    timeseries[indices, :] = profiles.astype(ts_dtype)
 
-        return DatasetInfo(**metainfo)
+    return timeseries.T
+
+
+def _extract_info(data: tp.Any) -> DatasetInfo:
+    metainfo: dict[str, tp.Any] = json.loads(str(data["metadata"][0]))
+
+    metainfo["radii"] = np.asarray(metainfo["radii"], dtype=np.int32)
+    metainfo["profiles"] = np.asarray(data["profiles"], dtype=np.float32)
+    metainfo["valid_mask"] = np.asarray(metainfo["valid_mask"], dtype=np.bool_)
+    metainfo["timeseries_shape"] = tuple(metainfo["timeseries_shape"])
+
+    return DatasetInfo(**metainfo)
